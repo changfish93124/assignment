1d56efccd78bdd8c200cf5191ae31751e92b8afb
changfish93124 <100886201+changfish93124@users.noreply.github.com>
2022-03-18 08:48:44 +0800

    good

--- print_variable.c
+++ print_variable.c
@@ -7,7 +7,7 @@ int main() {
         //give me some output messages in following format
         //and that can be done by add only ONE line code.
         //please refer to Stdio.h slides page 15.
-        
+
         //j=5, b=435
         //j=6, b=522
         //j=7, b=609
@@ -15,5 +15,6 @@ int main() {
         //j=9, b=783
         //...
         //j=22, b=1914
+        printf("j=%d, b=%d\n",j,b*j);
     }
 }
