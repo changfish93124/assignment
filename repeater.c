ac36208e70ebe851daa831884da6e4be9e3929fe
changfish93124 <100886201+changfish93124@users.noreply.github.com>
2022-03-18 08:56:23 +0800

    good

---
+++ repeater.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int input;
+
+    printf("每輸入一個數字，程式會再印出相同的數字。\n");
+    //fill some condition into the following while loop
+    //only ONE line code is you need
+    while (scanf("%d",&input)) {
+        printf("%d\n", input);
+    }
+}
