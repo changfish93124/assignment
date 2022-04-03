25f9893467110df23d698c4c2798e69edda992ec
changfish93124 <100886201+changfish93124@users.noreply.github.com>
2022-04-03 20:26:09 +0800

    記得用lf 還有double =P

---
+++ polynomial_evaluation.c
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+
+int main(){
+    double x,sum;
+    int y;
+    scanf("%lf%d",&x,&y);
+    double A[y];
+    int B[y];
+    for(int i=0 ;i<y;i++){
+        scanf("%lf%d",&A[i],&B[i]);
+        sum+=A[i]*pow(x,B[i]);
+    }
+    printf("%lf",sum);
+}
