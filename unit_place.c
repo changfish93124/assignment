b25a2b3328e29db7f996b788fb17771a60c0a948
changfish93124 <100886201+changfish93124@users.noreply.github.com>
2022-04-03 20:22:54 +0800

    輸出個位數、有註解、除錯.

---
+++ unit_place.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	char A[2];
+	while(scanf("%c",&A[0])){
+		if(A[0]=='\n'){
+			printf("%c",A[1]);  //A[0]是enter 所以print A[1]
+			break;	
+		}else{
+			if(A[0]>'0'&&A[0]<'9')
+				A[1]=A[0];  //每次皆把最後一位存在A[1]
+			else{
+				printf("輸入錯誤");  //如果不為數字 跳出
+				break;
+			}	
+		}		
+	}
+}
