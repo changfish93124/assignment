f7d3a42f7a7874615bb740ca523f09a36952d053
changfish93124 <100886201+changfish93124@users.noreply.github.com>
2022-04-22 08:58:52 +0800

    輸出個位數、有註解、除錯.

--- unit_place.c
+++ unit_place.c
@@ -7,7 +7,7 @@ int main() {
 			printf("%c",A[1]);  //A[0]是enter 所以print A[1]
 			break;	
 		}else{
-			if(A[0]>'0'&&A[0]<'9')
+			if(A[0]>='0'&&A[0]<='9')
 				A[1]=A[0];  //每次皆把最後一位存在A[1]
 			else{
 				printf("輸入錯誤");  //如果不為數字 跳出
